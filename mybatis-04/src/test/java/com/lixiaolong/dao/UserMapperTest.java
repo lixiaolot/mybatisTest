0f245cb419ed75278179762d91c3608cbb8c88f1
lixiaolot <2235212799@qq.com>
2022-09-27 21:52:16 +0800

    1

--- mybatis-04/src/test/java/com/lixiaolong/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/com/lixiaolong/dao/UserMapperTest.java
@@ -3,14 +3,20 @@ package com.lixiaolong.dao;
 import com.lixiaolong.pojo.User;
 import com.lixiaolong.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+
 public class UserMapperTest {
+
+   static Logger logger = Logger.getLogger(UserMapperTest.class);
 
     @Test
     public void test(){
 //        获取sqlsession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        logger.info("进入查询");
         //执行sql
         //方式1
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
@@ -22,5 +28,13 @@ public class UserMapperTest {
 
         //关闭Sqlsession
         sqlSession.close();
+    }
+
+    @Test
+    public void testlog4j(){
+        logger.info("info:进入了testLog4J");
+        logger.debug("debug:进入了testLog4J");
+        logger.error("error:进入了testLog4J");
+
     }
 }
